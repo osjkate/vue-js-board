3a83f9d19cf5012c9642127fc49fbbbe185a5a1f
osjkate <98140863+osjkate@users.noreply.github.com>
2023-02-17 16:46:52 +0900

    create와 read기능 구현

---
+++ vue-board/src/components/Create.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input v-model="writer" type="text" placeholder="글쓴이" />
+    <input v-model="title" type="text" placeholder="제목" />
+    <textarea
+      v-model="content"
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+      placeholder="내용"
+    ></textarea>
+    <button @click="write">작성</button>
+  </div>
+</template>
+
+<script>
+import data from "@/data";
+export default {
+  name: "Create",
+  data() {
+    return {
+      data: data,
+      writer: "",
+      title: "",
+      content: ""
+    };
+  },
+  methods: {
+    write() {
+      this.data.push({
+        writer: this.writer,
+        title: this.title,
+        content: this.content
+      });
+      this.$router.push({
+        path: "/"
+      });
+    }
+  }
+};
+</script>
+
+<style></style>
